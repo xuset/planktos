148836993f6c47119f99863a7fcda0a2e04984e5
George Farcasiu <farcasiu.george@gmail.com>
2017-01-15 17:21:34 -0600

    Removed var statements from bin scripts
    track #12

--- bin/setup.js
+++ bin/setup.js
@@ -4,18 +4,18 @@ module.exports = setup
 
 /* eslint no-path-concat: "off" */
 
-var path = require('path')
-var createTorrent = require('create-torrent')
-var crypto = require('crypto')
-var fs = require('fs')
-var minimist = require('minimist')
-var parallelLimit = require('run-parallel-limit')
+const createTorrent = require('create-torrent')
+const crypto = require('crypto')
+const fs = require('fs')
+const minimist = require('minimist')
+const parallelLimit = require('run-parallel-limit')
+const path = require('path')
 
-var RESERVED_DIR = 'planktos'
+const RESERVED_DIR = 'planktos'
 
 function copyLib (rootDir) {
   rootDir = absPath(rootDir)
-  var dstDir = rootDir + '/' + RESERVED_DIR
+  const dstDir = rootDir + '/' + RESERVED_DIR
   if (!fs.existsSync(dstDir)) fs.mkdirSync(dstDir)
   copyFile(__dirname + '/../install.js', dstDir + '/install.js')
   copyFile(__dirname + '/../build/planktos.min.js', dstDir + '/planktos.min.js')
@@ -27,7 +27,7 @@ function copyLib (rootDir) {
 function setup (rootDir, includes, webseedUrls) {
   rootDir = absPath(rootDir)
   includes = includes.map(p => absPath(p))
-  var dstDir = rootDir + '/' + RESERVED_DIR
+  const dstDir = rootDir + '/' + RESERVED_DIR
 
   if (includes.find(f => !isNested(rootDir, f))) {
     throw new Error('Included files must be inside the root directory')
@@ -38,11 +38,11 @@ function setup (rootDir, includes, webseedUrls) {
   copyAsHash(rootDir, includes, dstDir, function (err, mappings) {
     if (err) throw err
 
-    var torrentFiles = mappings.map(e => absPath(e.dst))
+    let torrentFiles = mappings.map(e => absPath(e.dst))
 
     // From BitTorrent Documentation: "In the single file case, the name key is the name of
     // a file, in the multiple file case, it's the name of a directory."
-    var opts = {
+    let opts = {
       urlList: webseedUrls,
       name: torrentFiles.length === 1 ? '/planktos/' + torrentFiles[0].slice(torrentFiles[0].lastIndexOf('/') + 1) : RESERVED_DIR
     }
@@ -59,30 +59,30 @@ function setup (rootDir, includes, webseedUrls) {
 }
 
 function writeManifest (srcDir, dstDir, mappings) {
-  var relMappings = {}
-  for (var map of mappings) {
+  let relMappings = {}
+  for (let map of mappings) {
     relMappings[map.src.substr(srcDir.length + 1)] = map.dst.substr(dstDir.length + 1)
   }
-  var buff = new Buffer(JSON.stringify(relMappings))
+  let buff = new Buffer(JSON.stringify(relMappings))
   fs.writeFileSync(dstDir + '/manifest.json', buff)
 }
 
 function copyAsHash (rootDir, srcList, dstDir, cb) {
-  var files = []
+  let files = []
 
-  for (var item of srcList) {
+  for (let item of srcList) {
     files = files.concat(walk(item).filter(f => !isNested(dstDir, f)))
   }
 
-  var tasks = files.map(fname => {
+  let tasks = files.map(fname => {
     return function (cc) { copyFileAsHash(fname, dstDir, cc) }
   })
   parallelLimit(tasks, 2, cb)
 }
 
 function getHash (src, cb) {
-  var hash = crypto.createHash('sha1')
-  var stream = fs.createReadStream(src)
+  let hash = crypto.createHash('sha1')
+  let stream = fs.createReadStream(src)
 
   stream.on('error', function (err) {
     cb(err)
@@ -100,7 +100,7 @@ function getHash (src, cb) {
 function copyFileAsHash (srcFile, dstDir, cb) {
   getHash(srcFile, function (err, hash) {
     if (err) return cb(err)
-    var dstFile = dstDir + '/' + hash
+    const dstFile = dstDir + '/' + hash
     copyFile(srcFile, dstFile, 'wx', function (err) {
       if (err) cb(err)
       else cb(null, {src: srcFile, dst: dstFile})
@@ -111,8 +111,8 @@ function copyFileAsHash (srcFile, dstDir, cb) {
 function copyFile (srcFile, dstFile, flags, cb) {
   if (typeof flags === 'function') return copyFile(srcFile, dstFile, null, flags)
   if (!cb) cb = noop
-  var read = fs.createReadStream(srcFile)
-  var write = fs.createWriteStream(dstFile, {flags: flags})
+  let read = fs.createReadStream(srcFile)
+  let write = fs.createWriteStream(dstFile, {flags: flags})
 
   read.on('error', function (err) {
     cb(err)
@@ -129,7 +129,7 @@ function copyFile (srcFile, dstFile, flags, cb) {
 }
 
 function walk (dir, filelist) {
-  var files = []
+  let files = []
   filelist = filelist || []
   try {
     files = fs.readdirSync(dir)
@@ -138,8 +138,8 @@ function walk (dir, filelist) {
     else throw err
   }
 
-  for (var file of files) {
-    var name = dir + '/' + file
+  for (let file of files) {
+    const name = dir + '/' + file
     if (fs.statSync(name).isDirectory()) {
       walk(name, filelist)
     } else {
@@ -172,7 +172,7 @@ function printHelp () {
 }
 
 if (require.main === module) {
-  var argv = minimist(process.argv.slice(2), {
+  const argv = minimist(process.argv.slice(2), {
     alias: {
       h: 'help',
       r: 'root',
@@ -187,9 +187,9 @@ if (require.main === module) {
     printHelp()
     process.exit(0)
   }
-  var rootDir = argv.root || process.cwd()
-  var includes = argv['_'].length === 0 ? [rootDir] : argv['_']
-  var webseedUrls = argv.webseed
+  const rootDir = argv.root || process.cwd()
+  const includes = argv['_'].length === 0 ? [rootDir] : argv['_']
+  const webseedUrls = argv.webseed
 
   if (argv['lib-only']) copyLib(rootDir)
   else setup(rootDir, includes, webseedUrls)
