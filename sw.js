8df10fd2873a7c3f7efc6c934384d249b1b03735
Austin Middleton <xuset1@gmail.com>
2017-02-05 10:23:43 -0600

    Simplify the service worker a bit
    The sw is now at 12 loc which is pretty awesome!

--- sw.js
+++ sw.js
@@ -7,30 +7,23 @@ importScripts('planktos/planktos.min.js')
 // The location of the planktos root directory
 const root = location.pathname.substring(0, location.pathname.lastIndexOf('/'))
 
-addEventListener('fetch', onFetch)
-addEventListener('install', onInstall)
-
-function onInstall (event) {
+addEventListener('install', function (event) {
   event.waitUntil(planktos.update(root))
-}
+})
 
-function onFetch (event) {
+addEventListener('fetch', function (event) {
   let url = new URL(event.request.url)
 
+  // Early return tells the browser to handle the request instead of the service worker
   if (url.host !== location.host || event.request.method !== 'GET') return
 
   // Let the browser handle webseed requests for performance reasons
   if (url.pathname.replace(root, '').startsWith('/planktos/files/')) return
 
-  console.log('PLANKTOS-FETCH', 'url=' + url.pathname)
-
-  // Fallback to browser http if the file was not found in the torrent or an error occurs
+  // Fallback to http if the file was not found in the torrent or an error occurs
   let responsePromise = planktos.fetch(event, {root: root})
-  .then(response => response != null ? response : fetch(event.request))
-  .catch(err => {
-    console.log('PLANKTOS-ERROR', err)
-    return fetch(event.request)
-  })
+    .catch(err => console.log('PLANKTOS-ERROR', err))
+    .then(response => response != null ? response : fetch(event.request))
 
   event.respondWith(responsePromise)
-}
+})
