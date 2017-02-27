6574148530a14b7e01ee6aa3544db68cbba0a003
George Farcasiu <farcasiu.george@gmail.com>
2017-02-27 11:24:45 -0600

    Added a source map preprocessor to karma

--- karma.conf.js
+++ karma.conf.js
@@ -25,6 +25,7 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '**/*.js': ['sourcemap']
     },
 
     // test results reporter to use
